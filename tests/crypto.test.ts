2303760aafa4e2a5d7ed3047b1406a23645ab81b
subframe7536 <1667077010@qq.com>
2023-06-21 10:08:05 +0800

    test: add test for aes

--- tests/crypto.test.ts
+++ tests/crypto.test.ts
@@ -1,13 +1,21 @@
 import { Buffer } from 'node:buffer'
 import { describe, expect, test } from 'vitest'
-import { generateRSA, signature, verify } from '../src/crypto'
+import { decrypt, encrypt, generateKey, generateRSA, signature, verify } from '../src/crypto'
 
-describe('test crypto', () => {
-  let base = ''
-  for (let i = 0; i < 1_000; i++) {
-    base += 'hello+world'
-  }
-  const buffer = Buffer.from(base, 'utf-8')
+let plain = ''
+for (let i = 0; i < 1_000; i++) {
+  plain += 'hello+world'
+}
+describe('test aes', async () => {
+  const key = generateKey(Buffer.from(plain, 'utf-8'), 'crypto test', 32)
+  const iv = generateKey(Buffer.from(plain, 'utf-8'), 'crypto test', 16)
+  test('test', async () => {
+    const e = encrypt(plain, key, iv)
+    expect(decrypt(e, key, iv)).toBe(plain)
+  })
+})
+describe('test verify', () => {
+  const buffer = Buffer.from(plain, 'utf-8')
   const { privateKey, publicKey } = generateRSA()
   const name = 'crypto test'
   const sig = signature(buffer, privateKey, publicKey, name)
