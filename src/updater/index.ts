653ad27292951db94bcea245d67e6a39f85d8de9
subframe7536 <1667077010@qq.com>
2024-07-15 10:44:55 +0800

    revert: restore createUpdater function

--- src/updater/index.ts
+++ src/updater/index.ts
@@ -1,4 +1,16 @@
+import type { UpdaterOption } from './types'
+import { Updater } from './core'
+
 export * from './core'
 export * from './types'
 export * from './defaultFunctions/compareVersion'
 export * from './defaultFunctions/download'
+
+/**
+ * create updater instance
+ * @param option updater option
+ * @returns updater
+ */
+export function createUpdater(option?: UpdaterOption) {
+  return new Updater(option)
+}
