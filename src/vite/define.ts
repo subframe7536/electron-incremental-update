b6521c270038940331bd4e8970d09371a3ba759b
subframe7536 <1667077010@qq.com>
2025-06-26 15:04:42 +0800

    feat: add helper function `defineElectronConfig`

---
+++ src/vite/define.ts
@@ -0,0 +1,26 @@
+import type { ElectronWithUpdaterOptions } from './option'
+import type { UserConfig, UserConfigFn } from 'vite'
+
+import { electronWithUpdater } from './core'
+
+type MakeOptional<T, K extends keyof T> = Partial<Pick<T, K>> & Omit<T, K>
+
+interface ElectronViteHelperOptions extends MakeOptional<ElectronWithUpdaterOptions, 'isBuild'> {
+  /**
+   * Config for renderer process
+   */
+  renderer?: UserConfig
+}
+
+export function defineElectronConfig(
+  options: ElectronViteHelperOptions,
+): UserConfigFn {
+  return ({ command }) => {
+    options.isBuild ??= command === 'build'
+    const electronPlugin = electronWithUpdater(options as ElectronWithUpdaterOptions)
+    const result = options.renderer ?? {}
+    result.plugins ??= []
+    result.plugins.push(electronPlugin)
+    return result
+  }
+}
