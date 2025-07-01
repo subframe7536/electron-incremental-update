dfacce23018b32fc6971704026b3449290003ca6
subframe7536 <1667077010@qq.com>
2025-07-01 14:34:39 +0800

    docs: add jsdoc for `defineElectronConfig`

--- src/vite/define.ts
+++ src/vite/define.ts
@@ -12,6 +12,37 @@ export interface ElectronViteHelperOptions extends MakeOptional<ElectronWithUpda
   renderer?: UserConfig
 }
 
+/**
+ * Vite config helper
+ * @see {@link electronWithUpdater}
+ * @example
+ * ```ts
+ * import { defineElectronConfig } from 'electron-incremental-update/vite'
+ *
+ * export default defineElectronConfig({
+ *   main: {
+ *     files: ['./electron/main/index.ts', './electron/main/worker.ts'],
+ *     // see https://github.com/electron-vite/electron-vite-vue/blob/85ed267c4851bf59f32888d766c0071661d4b94c/vite.config.ts#L22-L28
+ *     onstart: debugStartup,
+ *   },
+ *   preload: {
+ *     files: './electron/preload/index.ts',
+ *   },
+ *   updater: {
+ *     // options
+ *   },
+ *   renderer: {
+ *     server: process.env.VSCODE_DEBUG && (() => {
+ *       const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
+ *       return {
+ *         host: url.hostname,
+ *         port: +url.port,
+ *       }
+ *     })(),
+ *   }
+ * })
+ * ```
+ */
 export function defineElectronConfig(
   options: ElectronViteHelperOptions,
 ): UserConfigFn {
