46fb69d59c144246e0a630f8507636a8ccf636ba
subframe7536 <1667077010@qq.com>
2025-07-31 13:16:24 +0800

    fix: use var in cjs shim

--- src/vite/esm/constant.ts
+++ src/vite/esm/constant.ts
@@ -5,17 +5,17 @@ const CJSShim = `
 import __cjs_url__ from 'node:url';
 import __cjs_path__ from 'node:path';
 import __cjs_mod__ from 'node:module';
-const __filename = __cjs_url__.fileURLToPath(import.meta.url);
-const __dirname = __cjs_path__.dirname(__filename);
-const require = __cjs_mod__.createRequire(import.meta.url);
+var __filename = __cjs_url__.fileURLToPath(import.meta.url);
+var __dirname = __cjs_path__.dirname(__filename);
+var require = __cjs_mod__.createRequire(import.meta.url);
 `
 
 const CJSShim_electron_30 = `
 // -- CommonJS Shims --
 import __cjs_mod__ from 'node:module';
-const __filename = import.meta.filename;
-const __dirname = import.meta.dirname;
-const require = __cjs_mod__.createRequire(import.meta.url);
+var __filename = import.meta.filename;
+var __dirname = import.meta.dirname;
+var require = __cjs_mod__.createRequire(import.meta.url);
 `
 
 export const shim = electronMajorVersion >= 30 ? CJSShim_electron_30 : CJSShim
